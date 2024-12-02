346cd3edcef26eb5ccaa5125ef37d3bd0b0b2bed
andjan3 <150336624+andjan3@users.noreply.github.com>
2024-12-02 12:05:30 +0100

    changed resend nv

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -1,6 +1,6 @@
 import { Resend } from 'resend'
 
-const resend = new Resend(process.env.RESEND)
+const resend = new Resend(process.env.RESEND_API)
 
 export async function POST(req: Request) {
   const { name, number, email, message } = await req.json()

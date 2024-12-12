63f7e3eac817d53875bcfafe3d9747c17064c735
andjan3 <150336624+andjan3@users.noreply.github.com>
2024-12-12 14:05:33 +0100

    changed api key and email

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -1,6 +1,6 @@
 import { Resend } from 'resend'
 
-const resend = new Resend("re_BZbnyodR_KByLeBLYmf3oS3kwsubNarK6")
+const resend = new Resend("re_fKcPmHZC_K3kxQSbiA8rKHmwWbdqHz46u")
 
 export async function POST(req: Request) {
   const { name, number, email, message } = await req.json()
@@ -15,9 +15,9 @@ export async function POST(req: Request) {
   `
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Notifikation: BK-Kontakt <onboarding@resend.dev>',
-      to: ['andrea@bastakompisar.se'],
-      subject: 'Test',
+      from: 'Notifikation: Kontakt <onboarding@resend.dev>',
+      to: ['David.andersson@kasi.se'],
+      subject: 'Meddelande',
       html: messageBody,
     })
 
